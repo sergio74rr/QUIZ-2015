e6733b1555b5aabf06249f037d83f5f512ca3f48
Sergio González Rueda <sergio74rr@gmail.com>
2015-04-30 17:36:23 +0200

    Diseño adaptable

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -22,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use(partials());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
